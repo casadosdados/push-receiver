2f1959a88d03094af5d42336cb7a0741d7828b29
Jhonata Menezes <jhonatamenezes10@gmail.com>
2019-07-08 13:52:53 -0300

    example app sinesp cidadao

--- example/sinesp-cidadao-app.js
+++ example/sinesp-cidadao-app.js
@@ -1,23 +1,21 @@
 const { register, listen } = require('../src/index.js');
 
 const appInfo = {
-    senderId   : '905942954488',
-    cert       : 'daf1d792d60867c52e39c238d9f178c42f35dd98',
-    appPackage : 'br.gov.sinesp.cidadao.android',
-
+  senderId   : '905942954488',
+  cert       : 'daf1d792d60867c52e39c238d9f178c42f35dd98',
+  appPackage : 'br.gov.sinesp.cidadao.android',
 };
 
 (async () => {
-    const credentials = await register(appInfo);
-    const fcmToken = credentials.fcm.token;
+  const credentials = await register(appInfo);
+  const fcmToken = credentials.fcm.token;
 
-    console.log('token', fcmToken);
-    const persistentIds = [];
-    await listen({ ...credentials, persistentIds}, onNotification);
+  console.log('token', fcmToken);
+  const persistentIds = [];
+  await listen({ ...credentials, persistentIds }, onNotification);
 
-    // Called on new notification
-    function onNotification({ notification }) {
+  // Called on new notification
+  function onNotification({ notification }) {
     console.log('notification', notification);
-    }
+  }
 })();
-
